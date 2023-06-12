016575c842088171453b44a087caca51d9aa845e
Shivani Anandan <shivanianandhan28@gmail.com>
2023-06-12 21:46:56 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let [name,setName]=useState("");
+
+  function handlechange(event){
+    setName(event.target.value);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+            
+        <input type="text" onChange={handlechange}/>
+        <p>Hello {name}!</p>
     </div>
   )
 }
